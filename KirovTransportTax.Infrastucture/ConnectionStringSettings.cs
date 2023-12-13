9c7926391fc29ec7efdc5714e34183c0162ae37f
elisaveta9 <lizashishova9@gmail.com>
2023-12-13 18:26:31 +0300

    Created connection to database

---
+++ KirovTransportTax.Infrastucture/ConnectionStringSettings.cs
@@ -0,0 +1,9 @@
+﻿using LinqToDB.Configuration;
+
+public class ConnectionStringSettings : IConnectionStringSettings
+{
+    public string ConnectionString { get; set; }
+    public string Name { get; set; }
+    public string ProviderName { get; set; }
+    public bool IsGlobal => false;
+}
