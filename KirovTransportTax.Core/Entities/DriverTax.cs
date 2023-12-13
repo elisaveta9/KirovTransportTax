12a6fba5a4afe9ffe8f69f658dc58a0b5d510dc6
elisaveta9 <lizashishova9@gmail.com>
2023-12-13 18:24:12 +0300

    Created Base Entities

---
+++ KirovTransportTax.Core/Entities/DriverTax.cs
@@ -0,0 +1,8 @@
+﻿namespace KirovTransportTax.Core.Entities
+{
+    public class DriverTax
+    {
+        public string DriverPassport { get; set; }
+        public int SumTax { get; set; }
+    }
+}
---
+++ KirovTransportTax.Core/Entities/TransportTaxRate.cs
@@ -0,0 +1,11 @@
+﻿namespace KirovTransportTax.Domain.Entities
+{
+    public class TransportTaxRate
+    {
+        public int Id { get; set; }
+        public string TransportType { get; set; }
+        public int MinHorsepower { get; set; }
+        public int MaxHorsepower { get; set; }
+        public int TaxRate { get; set; }
+    }
+}
