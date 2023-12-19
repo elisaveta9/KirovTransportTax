df8610661b1ee747dc78c34fc656889525db4501
elisaveta9 <lizashishova9@gmail.com>
2023-12-19 04:40:00 +0300

    Revert "Refactored structure"
    This reverts commit 484cb19a12649f07c245ee4eb8f83915fc62abbf.

---
+++ KirovTransportTax.Application/Interfaces/ITransportModelRepository.cs
@@ -0,0 +1,14 @@
+﻿using KirovTransportTax.Domain.Entities;
+
+namespace KirovTransportTax.Application.Interfaces
+{
+    public interface ITransportModelRepository : IRepository<TransportModel>
+    {
+        Task Create(TransportModel entity);
+        void Delete(TransportModel entity);
+        void Update(TransportModel entity);
+        void Update(string oldModelPK, TransportModel entity);
+
+        Task<IEnumerable<TransportModel>> GetModel(string modelPK);
+    }
+}
---
+++ KirovTransportTax.Core/Entities/TransportModel.cs
@@ -0,0 +1,11 @@
+﻿namespace KirovTransportTax.Domain.Entities
+{
+    public class TransportModel
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public int Horsepower { get; set; }
+        public int ReleaseYear { get; set; }
+        public string TransportType { get; set; } 
+    }
+}
