11c0c50257cf783ce76c42122ad1ec56e599f531
elisaveta9 <lizashishova9@gmail.com>
2023-12-22 17:54:54 +0300

    Add Setup Api

--- KirovTransportTax.API/Program.cs
+++ KirovTransportTax.API/Program.cs
@@ -1,37 +1,17 @@
-using Microsoft.AspNetCore.Authentication.Negotiate;
+using KirovTransportTax.API;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
-   .AddNegotiate();
-
-builder.Services.AddAuthorization(options =>
+internal class Program
 {
-    // By default, all incoming requests will be authorized according to the default policy.
-    options.FallbackPolicy = options.DefaultPolicy;
-});
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+    
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ KirovTransportTax.API/Startup.cs
@@ -0,0 +1,62 @@
+﻿using KirovTransportTax.Application.Interfaces.Repositories;
+using KirovTransportTax.Infrastucture;
+using KirovTransportTax.Infrastucture.Repositories;
+using LinqToDB;
+using LinqToDB.AspNet;
+using LinqToDB.AspNet.Logging;
+
+namespace KirovTransportTax.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddLinqToDBContext<TransportDbConnection>((provider, options) =>
+                options
+                .UsePostgreSQL(Configuration.GetConnectionString("PostgreSQL"))
+                .UseDefaultLogging(provider));
+
+            services.AddScoped<IBrandRepository, BrandRepository>();
+            services.AddScoped<IDriverRepository, DriverRepository>();
+            services.AddScoped<IDriverTaxRepository, DriverTaxRepository>();
+            services.AddScoped<ITransportModelRepository, TransportModelRepository>();
+            services.AddScoped<ITransportRepository, TransportRepository>();
+            services.AddScoped<ITransportTaxRepository, TransportTaxRepository>();
+
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
+                options.JsonSerializerOptions.Converters.Add(new NullableDateOnlyJsonConverter());
+                options.JsonSerializerOptions.IncludeFields = true;
+            });
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
